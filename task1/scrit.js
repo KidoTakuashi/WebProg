7477b419d5bdac31d7719023bf2557e1b6b53b64
KidoTakuashi <mrloggin16@gmail.com>
2025-01-09 14:18:40 +0200

    task1
    test1

---
+++ task1/scrit.js
@@ -0,0 +1,23 @@
+var a = prompt("a");
+var b = prompt("b");
+var x = parseInt(a);
+var y = parseInt(b);
+
+alert((x + y) + "\n" + (x - y) + "\n" + (x * y) + "\n" + (x / y));
+console.log((x + y) + "\n" + (x - y) + "\n" + (x * y) + "\n" + (x / y));
+
+
+var a = prompt("a");
+var b = prompt("b");
+
+var x = parseInt(a);
+var y = parseInt(b);
+
+var sum = 0;
+for (var i = x; i <= y; i++) {
+  sum += i;
+}
+
+alert(sum);
+
+console.log(sum)
